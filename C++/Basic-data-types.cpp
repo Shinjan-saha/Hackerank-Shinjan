05b882c5c728aefbe0d4bf19810e95693e5322ce
Shinjan saha <116014977+Shinjan-saha@users.noreply.github.com>
2023-06-17 11:28:31 +0530

    Create Basic-data-types.cpp

---
+++ C++/Basic-data-types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    int long b;
+    char c;
+    float d;
+    double e;
+   scanf("%d",&a,&e);
+   scanf("%c",c);
+   scanf("%f",&d);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    // Complete the code.
+    return 0;
+}
